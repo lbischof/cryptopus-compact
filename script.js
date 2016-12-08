c7da99d2759ddd80617e04b871a1a54c02fdccbb
Lorenz Bischof <lorenzbischof@gmail.com>
2016-12-08 19:31:28 +0100

    optimize for new cryptopus version

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.documentElement.id = 'custom'
+document.body.addEventListener("mouseover",function(e) {
+    if (e.target.classList.contains('result-small')) {
+        var delay = setTimeout(function() { e.target.click() }, 300);
+        this.onmouseout = function() { clearTimeout(delay) };
+    }
+});
